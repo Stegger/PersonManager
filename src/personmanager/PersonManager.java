b453a4de1c44db80bc852e7daffc6fd378c50771
Stegger <stegger.peter@gmail.com>
2019-10-21 13:32:03 +0200

    Update PersonManager.java
    Changed Stegger to Trine

--- src/personmanager/PersonManager.java
+++ src/personmanager/PersonManager.java
@@ -23,7 +23,7 @@ public class PersonManager
         
         String[] arrPersons = new String[10];
         
-        arrPersons[0] = "Stegger";
+        arrPersons[0] = "Trine";
         
         for (int i = 0; i < arrPersons.length; i++)
         {
